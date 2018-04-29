c390d0ef1210d77cd4da4d3c863c6d63253cd298
harshasastry <harsha.sastry@gmail.com>
2018-04-29 13:43:27 +0530

    some js changes

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,39 +1,48 @@
 jQuery(function()
 {
     return $('#organisation').change(function()
-   {
-      var cities;
-      cities = $('#city').html();
-   	   $('#city').empty();
-       var org, options;
-       org = $('#organisation :selected').text();
-       options = $(cities).filter("optgroup[label=" + org + "]").html();
-
-		    if (options)
-		    {
-		      return $('#city').html(options);
-		    }
-		    else
-		    {
-		      return $('#city').empty();
-		    }
+    {
+        var cities;
+        var org, options;
+        cities = $('#city').html();
+        console.log("cities = " +cities);
+        //$('#city').empty();
+        options = '<option value="">Select City</option>';
+        org = $('#organisation :selected').text();
+        escaped_org= org.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1');
+        options = options + $(cities).filter("optgroup[label=" + escaped_org + "]").html();
+        console.log("options = " +options);
+        if (options)
+        {
+          return $('#city').html(options);
+        }
+        else
+        {
+          return $('#city').empty();
+        }
   	});
 });
 
 jQuery(function()
 {
-    return $('#city').change(function() {
-     var locations;
-     locations = $('#location').html();
-       $('#location').empty();
-       var cty, options;
-       cty = $('#city :selected').text();
-       options = $(locations).filter("optgroup[label=" + cty + "]").html();
-       console.log("Options: "+options);
-        if (options) {
-          return $('#location').html(options);
-        } else {
-          return $('#location').empty();
+    return $('#city').change(function()
+    {
+        var locations;
+        locations = $('#location').html();
+        //$('#location').empty();
+        var cty, options;
+        options = '<option value="">Select Location</option>';
+        cty = $('#city :selected').text();
+        escaped_cty= cty.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1');
+        options = options + $(locations).filter("optgroup[label=" + escaped_cty + "]").html();
+        console.log("Options: "+options);
+        if (options)
+        {
+            return $('#location').html(options);
+        }
+        else
+        {
+            return $('#location').empty();
         }
     });
 });
@@ -42,18 +51,24 @@ jQuery(function()
 jQuery(function()
 {
 
-   	return $('#location').change(function() {
-     var units;
-     units = $('#unit').html();
-   	   $('#unit').empty();
-       var loc, options;
-       loc = $('#location :selected').text();
-       options = $(units).filter("optgroup[label=" + loc + "]").html();
-       console.log("Options: "+options);
-        if (options) {
-          return $('#unit').html(options);
-        } else {
-          return $('#unit').empty();
+   	return $('#location').change(function()
+    {
+        var units;
+        units = $('#unit').html();
+   	    //$('#unit').empty();
+        var loc, options;
+        options = '<option value="">Select Unit</option>';
+        loc = $('#location :selected').text();
+        escaped_loc= loc.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1');
+        options = options + $(units).filter("optgroup[label=" + escaped_loc + "]").html();
+        console.log("Options: "+options);
+        if (options)
+        {
+            return $('#unit').html(options);
+        }
+        else
+        {
+            return $('#unit').empty();
         }
     });
 });
@@ -61,20 +76,24 @@ jQuery(function()
 //2
 jQuery(function()
 {
-   	return $('#unit').change(function() {
-     var towers;
-     towers = $('#tower').html();
-
-   	   $('#tower').empty();
-       var unt, options;
-       unt = $('#unit :selected').text();
-       escaped_unt= unt.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1');
-       options = $(towers).filter("optgroup[label=" + escaped_unt + "]").html();
-       console.log("Options: "+options);
-        if (options) {
-          return $('#tower').html(options);
-        } else {
-          return $('#tower').empty();
+   	return $('#unit').change(function()
+    {
+        var towers;
+        towers = $('#tower').html();
+     	  //$('#tower').empty();
+        var unt, options;
+        options = '<option value="">Select Tower</option>';
+        unt = $('#unit :selected').text();
+        escaped_unt= unt.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1');
+        options = options + $(towers).filter("optgroup[label=" + escaped_unt + "]").html();
+        console.log("Options: "+options);
+        if (options)
+        {
+            return $('#tower').html(options);
+        }
+        else
+        {
+            return $('#tower').empty();
         }
     });
 });
@@ -82,19 +101,24 @@ jQuery(function()
 //3
 jQuery(function()
 {
-   	return $('#tower').change(function() {
-     var floors;
-     floors = $('#floor').html();
-   	   $('#floor').empty();
-       var twr, options;
-       twr = $('#tower :selected').text();
-       escaped_twr= twr.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1');
-       options = $(floors).filter("optgroup[label=" + escaped_twr + "]").html();
-       console.log("Options: "+options);
-        if (options) {
-          return $('#floor').html(options);
-        } else {
-          return $('#floor').empty();
+   	return $('#tower').change(function()
+    {
+        var floors;
+        floors = $('#floor').html();
+   	    //$('#floor').empty();
+        var twr, options;
+        options = '<option value="">Select Floor</option>';
+        twr = $('#tower :selected').text();
+        escaped_twr= twr.replace(/([ #;&,.+*~\':"!^$[\]()=>|\/@])/g, '\\$1');
+        options = options + $(floors).filter("optgroup[label=" + escaped_twr + "]").html();
+        console.log("Options: "+options);
+        if (options)
+        {
+            return $('#floor').html(options);
+        }
+        else
+        {
+            return $('#floor').empty();
         }
     });
 });
